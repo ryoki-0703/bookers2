91a3aa8db3281c845b057dbbe31460ba6b73d249
ryoki-0703 <tacchimi504@gmail.com>
2021-01-23 04:49:40 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+ 
+  attachment :profile_image
+  
+  validates :name,
+    presence: true,
+    length: {minimum: 2, maximum: 20}
+    
+  validates :introduction, presence: false, length: { maximum: 50 }
+  
+  has_many :books,dependent: :destroy
+  
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f6bb1bb8b4cd78f5bafc9d4ff1fb5622bca5a352924bcc0b628079b4f32d9caa8b5302137838761b9945001dbff23180bdf181ef4b12e232afb6965351490666'
